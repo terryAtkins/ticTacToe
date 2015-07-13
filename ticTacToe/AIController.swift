b5ffcfae74ff0dbf483525ef24cfed6252f60787
terryAtkins <git2hubs@gmail.com>
2015-07-13 14:29:55 +0100

    miniMax v0.3

--- ticTacToe/AIController.swift
+++ ticTacToe/AIController.swift
@@ -28,14 +28,13 @@ public class AIController {
         
         for square in emptySquares {
             dummyBoard[square] = currentPlayer
-            if board.isWin(gameBoard: dummyBoard) {
+            if isFirstMoveAWinFor_cuurentPlayer(dummyBoard: dummyBoard) {
                 result += [(squareId: square, minMax: 10)]
                 break
             }
             
             if !board.isWin(gameBoard: dummyBoard) {
-                dummyBoard[square] = playerToBlock
-                if board.isWin(gameBoard: dummyBoard) {
+                if isFirstMoveAWinFor_opponent(dummyBoard: dummyBoard, square: square, playerToBlock: playerToBlock) {
                     result += [(squareId: square, minMax: 10)]
                     break
                 }
@@ -44,28 +43,9 @@ public class AIController {
             if square == emptySquares.last && !board.isWin(gameBoard: dummyBoard) {
                 result += [(squareId: square, minMax: 0)]
                 break
-            } else {
-                dummyBoard[square] = currentPlayer
                 
-                for remainingSquare in emptySquares {
-                    if dummyBoard[remainingSquare] == 0 {
-                        dummyBoard[remainingSquare] = switchedPlayerId
-                        if board.isWin(gameBoard: dummyBoard) {
-                            if switchedPlayerId == currentPlayer {
-                                result += [(squareId: square, minMax: 1)]
-                                break
-                            } else {
-                                result += [(squareId: square, minMax: -1)]
-                                break
-                            }
-                        } else if remainingSquare == emptySquares.last && !board.isWin(gameBoard: dummyBoard) {
-                            result += [(squareId: square, minMax: 0)]
-                            break
-                        }
-                    }
-                    
-                    switchedPlayerId = switchedPlayerId == 1 ? 2 : 1
-                }
+            } else {
+                result += findTheBestSquareToPlay(dummyBoard: dummyBoard, emptySquares: emptySquares, square: square, currentPlayer: currentPlayer, switchedPlayerId: switchedPlayerId)
             }
             
             dummyBoard = currentBoard
@@ -73,10 +53,44 @@ public class AIController {
         result.sort { $0.1 > $1.1 }
         return result.first!.squareId
     }
+
     
+    func isFirstMoveAWinFor_cuurentPlayer(var #dummyBoard: [Int]) -> Bool {
+        return board.isWin(gameBoard: dummyBoard)
+    }
+    
+    func isFirstMoveAWinFor_opponent(var #dummyBoard: [Int], square: Int, playerToBlock: Int) -> Bool {
+        dummyBoard[square] = playerToBlock
+        return board.isWin(gameBoard: dummyBoard)
+    }
+    
+    func findTheBestSquareToPlay(var #dummyBoard: [Int], emptySquares: [Int], square: Int, currentPlayer: Int, var switchedPlayerId: Int) -> [(squareId: Int, minMax: Int)] {
+        dummyBoard[square] = currentPlayer
+        var squareScore: [(squareId: Int, minMax: Int)] = []
+        for remainingSquare in emptySquares {
+            if dummyBoard[remainingSquare] == 0 {
+                dummyBoard[remainingSquare] = switchedPlayerId
+                if board.isWin(gameBoard: dummyBoard) {
+                    if switchedPlayerId == currentPlayer {
+                        squareScore += [(squareId: square, minMax: 1)]
+                        break
+                    } else {
+                        squareScore += [(squareId: square, minMax: -1)]
+                        break
+                    }
+                    
+                } else if remainingSquare == emptySquares.last && !board.isWin(gameBoard: dummyBoard) {
+                    squareScore += [(squareId: square, minMax: 0)]
+                    break
+                }
+            }
+            switchedPlayerId = switchedPlayerId == 1 ? 2 : 1
+        }
+        return squareScore
+    }
 }
 
-          
+
 
 
 
